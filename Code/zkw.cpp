3f1453724557a25eec3acdc736d25f38bc88c2fe
cirno-baka <cirno_baka@foxmail.com>
2020-11-26 21:24:08 +0800

    ...

--- Code/zkw.cpp
+++ Code/zkw.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cstdio>
+#include <stack>
 typedef long long ll;
+using std::stack;
 
-ll n, m, bit, tree[2000009];
+ll n, m, bit, tree[2000009], ls[2000009], rs[2000009], tag[2000009];
 
 inline ll read()
 {
@@ -41,22 +43,50 @@ inline void build()
     for(register int i=bit+1; i<=bit+n; ++i)
     {
         tree[i]=read();
+        ls[i]=i-bit;
+        rs[i]=i-bit;
     }
     for(register int i=bit-1; i>=1; --i)
     {
-        maintain(i);
+        tree[i]=tree[(i<<2)]+tree[(i<<2|1)];
+        ls[i]=ls[(i<<2)];
+        rs[i]=rs[(i<<2|1)];
     }
     return;
 }
 
-inline void singlePointMod(ll pos, ll as)
+inline void pushDown(ll xx)
 {
-    pos+=bit;
-    tree[pos]=as;
-    for((pos>>=1); pos>=1; (pos>>=1))
+    if(tag[xx]>0&&xx>bit)
     {
-        maintain(pos);
+        tag[(xx<<1)]+=tag[xx];
+        tag[(xx<<1|1)]+=tag[xx];
+        tree[(xx<<1)]+=(rs[(xx<<1)]-ls[(xx<<1)])*tag[xx];
+        tree[(xx<<1|1)]+=(rs[(xx<<1|1)]-ls[(xx<<1|1)])*tag[xx];
+        tag[xx]=0;
     }
+    return;
+}
+
+inline void tagUpdate(ll nod)
+{
+    stack <ll> s;
+    while(nod>0)
+    {
+        s.push(nod);
+        nod>>=1;
+    }
+    while(s.size>0)
+    {
+        pushDown(s.top);
+        s.pop();
+    }
+    return;
+}
+
+inline void rangeUpdate(ll pl, ll pr, ll as)
+{
+    int 
     return;
 }
 
@@ -85,13 +115,14 @@ int main()
     build();
     for(register ll i=1; i<=m; ++i)
     {
-        ll o, x, y;
+        ll o, x, y, k;
         o=read();
         if(o==1)
         {
             x=read();
             y=read();
-            singlePointMod(x, tree[x+bit]+y);
+            k=read();
+            rangeUpdate(x, y, k);
         }
         else
         {
